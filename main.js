3260366214e1b6b6b97d4ef284f2ade89658da3f
r-dog <Jake.kgrogan@gmail.com>
2018-04-25 00:07:38 +0100

    added index

---
+++ main.js
@@ -0,0 +1,53 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const path = require('path');
+const url = require('url');
+const shell = require('electron').shell;
+
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+    }));
+
+    win.on('closed', () => {
+        win = null;
+    });
+
+    var menu = Menu.buildFromTemplate([
+        {
+            label: 'Menu',
+            submenu: [
+                {
+                    label: 'Adjust Notification Value'
+                },
+                {
+                    label: 'View Source',
+                    click() {
+                        shell.openExternal('https://github.com/r-dog/crypto-alert');
+                    }
+                },
+                {
+                    label: 'Exit',
+                    click() {
+                        app.quit()
+                    }
+                }
+            ]
+        }
+    ])
+
+    Menu.setApplicationMenu(menu);
+}
+
+app.on('ready', createWindow);
+
+app.on('active', () => {
+    if(win === null){
+        createWindow()
+    }
+});
